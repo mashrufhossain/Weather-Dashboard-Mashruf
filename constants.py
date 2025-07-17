d3810f87a7db47fc39ffff610ccf7060754b3b1f
Mashruf <mashrufhossain123@gmail.com>
2025-07-16 21:24:34 -0400

    Changed footer description for history tab

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-HISTORY_FOOTER = "This tab shows all previous weather history entries. Columns can be sorted by clicking their headers."
-STATS_FOOTER = "This tab summarizes weather history with key statistics and records, calculated using SQL."
+HISTORY_FOOTER = "This tab shows the 50 most recent weather history entries. Columns can be sorted by clicking their headers."
+STATS_FOOTER = "This tab summarizes all weather history with key statistics and records, calculated using SQL."
 FORECAST_FOOTER = "This tab displays predictive forecasts for the city entered."
